ceee3a61de5b14993cde21895fe7cf4c7eb7de76
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-01-06 18:52:42 +0200

    Design the Home Screen

--- lib/uis/screens/home_screen.dart
+++ lib/uis/screens/home_screen.dart
@@ -12,12 +12,43 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('GitHub'),
       ),
-      body: Center(
-        child: ElevatedButton(
-          child: const Text('Trigger'),
-          onPressed: Provider.of<Repositories>(context, listen: false)
-              .fetchRepositories,
-        ),
+      body: FutureBuilder(
+        future: Provider.of<Repositories>(context, listen: false)
+            .fetchRepositories(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          if (snapshot.error != null) {
+            return Center(child: Text('${snapshot.error}'));
+          } else {
+            return Consumer<Repositories>(
+              builder: (context, repositoriesProvider, _) => ListView.builder(
+                itemCount: repositoriesProvider.repositories.length,
+                itemBuilder: (context, index) {
+                  final currentRepo = repositoriesProvider.repositories[index];
+                  return Card(
+                    child: ListTile(
+                      leading: Image.network(currentRepo.owner.avatarUrl),
+                      title: Text(currentRepo.name),
+                      subtitle: Text(currentRepo.description),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Icon(
+                            Icons.star_rounded,
+                            color: Colors.amber,
+                          ),
+                          Text('${currentRepo.stars}'),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              ),
+            );
+          }
+        },
       ),
     );
   }
