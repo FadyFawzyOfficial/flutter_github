a1d956ce009c9f3245876eadde95ad8cce968a67
Fady Fawzy <FadyFawzy.Official@gmail.com>
2022-01-06 15:57:39 +0200

    Build Repositories Provider & Add a method to fetch repositories from GitHub

---
+++ lib/constants/strings.dart
@@ -0,0 +1,4 @@
+// Urls
+const baseUrl = 'api.github.com';
+const serachUrlPath = '/search';
+const searchRepositoriesUrlPath = '$serachUrlPath' '/repositories';
---
+++ lib/providers/repositories.dart
@@ -0,0 +1,36 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
+import '../constants/strings.dart';
+import '../models/repository.dart';
+
+class Repositoires extends ChangeNotifier {
+  List<Repository> _repositories = [];
+
+  List<Repository> get repositories => _repositories;
+
+  Future<void> fetchRepositories({String? searchKeyWord}) async {
+    final queryParameters = {'q': searchKeyWord ?? 'flutter'};
+    final uri = Uri.https(baseUrl, searchRepositoriesUrlPath, queryParameters);
+    debugPrint(uri.toString());
+
+    try {
+      final response = await http.get(
+        uri,
+        headers: {'Content-Type': 'application/json'},
+      );
+
+      if (response.statusCode == 200) {
+        debugPrint(response.body);
+        final Map<String, dynamic> responseBody = json.decode(response.body);
+        _repositories = List<Repository>.from(
+            responseBody['items'].map((x) => Repository.fromMap(x)));
+        notifyListeners();
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
+}
